908d91fdd72ee69917d1e0b2d6102a67876accd0
nathanpasdutout <nathanbaudrier@gmail.com>
2025-09-23 11:10:21 +0200

    Utils class updated

--- src/main/java/fr/nathanpasdutout/utils/Utils.java
+++ src/main/java/fr/nathanpasdutout/utils/Utils.java
@@ -1,8 +1,23 @@
 package fr.nathanpasdutout.utils;
 
+import java.time.OffsetDateTime;
+
 public class Utils {
 
     public static String capitalizeFirstLetter(String content) {
         return content.substring(0, 1).toUpperCase() + content.substring(1).toLowerCase();
+    }
+
+    public static String getDateFormat(String date) {
+        OffsetDateTime dateTime = OffsetDateTime.parse(date);
+        return Utils.capitalizeFirstLetter(dateTime.getMonth().name())
+                + " "
+                + dateTime.getDayOfMonth()
+                + ", at "
+                + dateTime.getHour()
+                + ":"
+                + dateTime.getMinute()
+                + ":"
+                + dateTime.getSecond();
     }
 }
